d509efcec37386504d9cc7687b3c96042617de87
Arpitha Rajeev <108493146+Arpitha-Rajeev1@users.noreply.github.com>
2023-11-03 00:22:49 +0100

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,38 @@
+import express from 'express'; // ES6 module syntax
+// const express = require('express'); for CommonJS module syntax
+import { PORT, mongoDBURL } from './config.js'; // ES6 module syntax
+// const { PORT } = require('./config'); for CommonJS module syntax
+import mongoose from 'mongoose';
+import cors from 'cors';
+import booksRoute from './routes/booksRoute.js';
+
+// create an instance of the express application
+const app = express();
+
+// middleware for parsing request body
+app.use(express.json());
+
+// allow all origins with default *
+app.use(cors());
+
+// OR - allow only 3000 port
+// app.use(
+//     cors({
+//         origin: 'http://localhost:3000',
+//         methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//         allowedHeaders: ['Content-Type']
+//     })
+// )
+
+app.use('/books', booksRoute);
+
+// mongoose is a Object Data Modeling library for MongoDB in Node.js, connect() uses Promise based approach
+mongoose.connect(mongoDBURL).then(() => {
+    console.log('Connected to Database');
+    // listen is a function of Express to start a server and listen to incoming connections with a callback function as second argument that gets executed when server starts
+    app.listen(PORT, () => {
+        console.log(`App is running on the port: ${PORT}`);
+    })
+}).catch((error) => {
+    console.log(error);
+})
