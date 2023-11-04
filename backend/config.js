9c44164002e415c4f15e916889496366f7632246
Arpitha Rajeev <108493146+Arpitha-Rajeev1@users.noreply.github.com>
2023-11-04 10:00:56 +0100

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 export const PORT = 8000; // ES6 module syntax
 // module.exports.PORT = 8000; for CommonJS module syntax
 
-export const mongoDBURL = 'mongodb+srv://arpitharajeev37:Handispace1$@cluster0.jcof70d.mongodb.net/?retryWrites=true&w=majority';
+export const mongoDBURL = 'YOUR URL';
 
